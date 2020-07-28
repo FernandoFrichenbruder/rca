5334c7b2c7f320a831b93c18b3adbaaa2246c8b3
Fernando <fernando@teky.com.br>
2020-07-27 22:40:10 -0300

    Refatorando o Cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -11,7 +11,21 @@ export class CartService {
   ) { }
 
   addToCart(product) {
-    this.items.push(product);
+    if(this.items.length > 0){
+          if (this.items.some(item => item.product.id == product.id )) {
+            for(var i = 0; i < this.items.length; i++){
+              if(this.items[i].product.id == product.id){
+                this.items[i].amount += 1;
+              }
+            }
+          } else {
+            this.items.push({ amount: 1, product});
+          }
+
+    } else {
+      this.items.push({ amount: 1, product});
+    }
+    console.log(this.items);
   }
 
   getItems() {
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,6 +12,7 @@ export class ProductsComponent implements OnInit {
 
   products = products;
   amount: number;
+  toggle: boolean = false;
 
   constructor(
     private cartService: CartService
